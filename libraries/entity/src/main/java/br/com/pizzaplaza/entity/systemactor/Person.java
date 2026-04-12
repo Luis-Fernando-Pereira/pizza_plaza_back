7440715cf541b4c42c01602771535f8390057319
Luis Fernando <pereiraluisfernando.01.98@gmail.com>
2026-04-11 22:33:01 -0300

    Adicionado anotação lombok a classe

--- libraries/entity/src/main/java/br/com/pizzaplaza/entity/systemactor/Person.java
+++ libraries/entity/src/main/java/br/com/pizzaplaza/entity/systemactor/Person.java
@@ -3,7 +3,9 @@ package br.com.pizzaplaza.entity.systemactor;
 import br.com.pizzaplaza.entity.fatherofall.Odin;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
 
+@Data
 @MappedSuperclass
 public class Person extends Odin {
 
